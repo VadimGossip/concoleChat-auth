b40aa5d392f27e9ce6b2bec47804b921b4d7ee7e
vadvoronin@gmail.com <greendarkroom90>
2024-08-23 15:17:58 +0300

    week 6

--- internal/api/auth/refresh.go
+++ internal/api/auth/refresh.go
@@ -2,10 +2,11 @@ package auth
 
 import (
 	"context"
+
 	desc "github.com/VadimGossip/concoleChat-auth/pkg/auth_v1"
 )
 
-func (s *Implementation) RefreshToken(ctx context.Context, req *desc.GetRefreshTokenRequest) (*desc.GetRefreshTokenResponse, error) {
+func (s *Implementation) GetRefreshToken(ctx context.Context, req *desc.GetRefreshTokenRequest) (*desc.GetRefreshTokenResponse, error) {
 	refreshToken, err := s.authService.RefreshToken(ctx, req.RefreshToken)
 	if err != nil {
 		return nil, err
