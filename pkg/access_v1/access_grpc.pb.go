e4269c006ed67686d877fccb612036b4043ac57d
vadvoronin@gmail.com <greendarkroom90>
2024-08-23 16:06:02 +0300

    week 6

--- pkg/access_v1/access_grpc.pb.go
+++ pkg/access_v1/access_grpc.pb.go
@@ -23,7 +23,7 @@ const _ = grpc.SupportPackageIsVersion7
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type AccessV1Client interface {
-	Create(ctx context.Context, in *CheckRequest, opts ...grpc.CallOption) (*emptypb.Empty, error)
+	Check(ctx context.Context, in *CheckRequest, opts ...grpc.CallOption) (*emptypb.Empty, error)
 }
 
 type accessV1Client struct {
@@ -34,9 +34,9 @@ func NewAccessV1Client(cc grpc.ClientConnInterface) AccessV1Client {
 	return &accessV1Client{cc}
 }
 
-func (c *accessV1Client) Create(ctx context.Context, in *CheckRequest, opts ...grpc.CallOption) (*emptypb.Empty, error) {
+func (c *accessV1Client) Check(ctx context.Context, in *CheckRequest, opts ...grpc.CallOption) (*emptypb.Empty, error) {
 	out := new(emptypb.Empty)
-	err := c.cc.Invoke(ctx, "/access_v1.AccessV1/Create", in, out, opts...)
+	err := c.cc.Invoke(ctx, "/access_v1.AccessV1/Check", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -47,7 +47,7 @@ func (c *accessV1Client) Create(ctx context.Context, in *CheckRequest, opts ...g
 // All implementations must embed UnimplementedAccessV1Server
 // for forward compatibility
 type AccessV1Server interface {
-	Create(context.Context, *CheckRequest) (*emptypb.Empty, error)
+	Check(context.Context, *CheckRequest) (*emptypb.Empty, error)
 	mustEmbedUnimplementedAccessV1Server()
 }
 
@@ -55,8 +55,8 @@ type AccessV1Server interface {
 type UnimplementedAccessV1Server struct {
 }
 
-func (UnimplementedAccessV1Server) Create(context.Context, *CheckRequest) (*emptypb.Empty, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method Create not implemented")
+func (UnimplementedAccessV1Server) Check(context.Context, *CheckRequest) (*emptypb.Empty, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Check not implemented")
 }
 func (UnimplementedAccessV1Server) mustEmbedUnimplementedAccessV1Server() {}
 
@@ -71,20 +71,20 @@ func RegisterAccessV1Server(s grpc.ServiceRegistrar, srv AccessV1Server) {
 	s.RegisterService(&AccessV1_ServiceDesc, srv)
 }
 
-func _AccessV1_Create_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _AccessV1_Check_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(CheckRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(AccessV1Server).Create(ctx, in)
+		return srv.(AccessV1Server).Check(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/access_v1.AccessV1/Create",
+		FullMethod: "/access_v1.AccessV1/Check",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(AccessV1Server).Create(ctx, req.(*CheckRequest))
+		return srv.(AccessV1Server).Check(ctx, req.(*CheckRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
@@ -97,8 +97,8 @@ var AccessV1_ServiceDesc = grpc.ServiceDesc{
 	HandlerType: (*AccessV1Server)(nil),
 	Methods: []grpc.MethodDesc{
 		{
-			MethodName: "Create",
-			Handler:    _AccessV1_Create_Handler,
+			MethodName: "Check",
+			Handler:    _AccessV1_Check_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
