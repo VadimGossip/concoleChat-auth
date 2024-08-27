6a32222c60941b7bf0df22aeda348f692f811911
vadvoronin@gmail.com <greendarkroom90>
2024-08-27 16:59:35 +0300

    week6

--- internal/app/app.go
+++ internal/app/app.go
@@ -2,7 +2,6 @@ package app
 
 import (
 	"context"
-	"log"
 	"net/http"
 	"os"
 	"os/signal"
@@ -11,7 +10,6 @@ import (
 	"time"
 
 	"github.com/VadimGossip/platform_common/pkg/closer"
-	"github.com/joho/godotenv"
 	"github.com/sirupsen/logrus"
 	"google.golang.org/grpc"
 
@@ -49,7 +47,6 @@ func NewApp(ctx context.Context, name string, appStartedAt time.Time) (*App, err
 
 func (a *App) initDeps(ctx context.Context) error {
 	inits := []func(context.Context) error{
-		a.initEnv,
 		a.initServiceProvider,
 		a.initGRPCServer,
 		a.initHTTPServer,
@@ -67,14 +64,6 @@ func (a *App) initDeps(ctx context.Context) error {
 
 func (a *App) initServiceProvider(_ context.Context) error {
 	a.serviceProvider = newServiceProvider()
-	return nil
-}
-
-func (a *App) initEnv(_ context.Context) error {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
 	return nil
 }
 
